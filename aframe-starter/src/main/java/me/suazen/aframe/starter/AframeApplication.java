dfa9e52e313196a610ccd312f564c3b8fcba398e
sujizhen <479428129@qq.com>
2023-04-04 18:46:51 +0800

    init

---
+++ aframe-starter/src/main/java/me/suazen/aframe/starter/AframeApplication.java
@@ -0,0 +1,13 @@
+package me.suazen.aframe.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AframeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AframeApplication.class, args);
+    }
+
+}
